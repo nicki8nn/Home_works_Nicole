a414f8f57e373a9ef00c3c6a5a3c29dfc4a18a09
Nicole M. <hello@nicolemedvecka.com>
2026-02-15 17:07:31 +0100

    updates

---
+++ theme_six/theme_six/second_task/main_seond.py
@@ -0,0 +1,48 @@
+import pathlib
+from typing import List, Dict
+
+def get_cats_info(path: str) -> List[Dict[str, str]]:
+    cats_list = []
+    try:
+        # 1. Працюємо з об'єктом Path
+        file_path = pathlib.Path(path)
+        
+        # 2. Перевірка на існування файлу
+        if not file_path.exists():
+            print(f"Помилка: Файл за шляхом '{path}' не знайдено.")
+            return []
+
+        # 3. Читання файлу
+        with open(file_path, 'r', encoding='utf-8') as file:
+            for line in file:
+                # Кожен рядок треба очистити від невидимих символів переносу (strip)
+                line = line.strip() 
+                
+                if line:  # Якщо рядок не порожній
+                    # Розбиваємо дані. дані потрібні окремо
+                    cat_id, name, age = line.split(',')
+                    
+                    # Створюємо словник
+                    cat_dict = {
+                        "id": cat_id, 
+                        "name": name, 
+                        "age": age
+                    }
+                    # Додаємо в список
+                    cats_list.append(cat_dict)
+                    
+        return cats_list
+
+    except Exception as e:
+        print(f"Виникла помилка: {e}")
+        return []
+
+if __name__ == "__main__":
+    # Створюємо шлях до файлу (автоматично в папці зі скриптом)
+    path_to_file = pathlib.Path(__file__).parent / "text_second.txt"
+
+    # Викликаємо функцію та отримуємо результат
+    cats_info = get_cats_info(str(path_to_file))
+    
+    # Виводимо результат
+    print(cats_info)
